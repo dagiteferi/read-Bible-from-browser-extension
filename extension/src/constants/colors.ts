7fad0996515ce2e1ca5c45a33365dab637f0e100
dagi <dagiteferi2011@gmail.com>
2026-02-25 17:23:03 +0300

    added constants

---
+++ extension/src/constants/colors.ts
@@ -0,0 +1,22 @@
+export const COLORS = {
+  // Light Mode
+  bgCream: '#F9F3E7',
+  indigoDeep: '#1E3A5F',
+  amberWarm: '#C97C3D',
+  oliveMountain: '#5F6D5A',
+  burgundyCurtain: '#722F37',
+  textPrimary: '#2C2C2C',
+  textSecondary: '#5C5C5C',
+  borderLight: '#E8E0D5',
+
+  // Dark Mode
+  darkBg: '#2C2C2C',
+  darkSurface: '#3C3C3C',
+  darkText: '#F0F0F0',
+  darkTextSecondary: '#B0B0B0',
+  darkBorder: '#4C4C4C',
+
+  // Accents (same in dark mode but adjusted)
+  darkIndigo: '#2A4A70',
+  darkAmber: '#D88C4A',
+};
